2873c6563bf454a311685e81550911a77a2f5346
paxer2k <alex.arkhipov.7590@gmail.com>
2023-08-22 19:44:08 +0200

    init comm

---
+++ Interfaces/IReservationService.cs
@@ -0,0 +1,20 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    public interface IReservationService
+    {
+        public List<Reservation> GetAllReservations();
+
+        public Reservation GetReservationById(int id);
+
+        public List<Customer> GetCustomersByBook(Book book);
+
+        public List<Book> GetBooksByCustomer(Customer customer);
+    }
+}
---
+++ Services/CustomerService.cs
@@ -0,0 +1,41 @@
+﻿using Dals;
+using Interfaces;
+using Models;
+
+namespace Services
+{
+    public class CustomerService : ICustomerService
+    {
+        private CustomerDao customerDAO;
+
+        public CustomerService()
+        {
+            customerDAO = new CustomerDao();
+        }
+
+        public List<Customer> GetAllCustomers()
+        {
+            return customerDAO.GetAllCustomers();
+        }
+
+        public Customer GetCustomerById(int id)
+        {
+            return customerDAO.GetCustomerById(id);
+        }
+
+        public void InsertCustomer(Customer customer)
+        {
+            customerDAO.InsertCustomer(customer);
+        }
+
+        public void UpdateCustomer(int id, Customer customer)
+        {
+            customerDAO.UpdateCustomer(id, customer);
+        }
+
+        public void DeleteCustomer(int id)
+        {
+            customerDAO.DeleteCustomer(id);
+        }
+    }
+}
